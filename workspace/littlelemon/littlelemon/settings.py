60eacd48b83a59194dd4e5905a8722bd36bc0a3c
brad-sworm <brad.swormstedt@gmail.com>
2024-07-19 14:51:37 -0400

    authentication

--- workspace/littlelemon/littlelemon/settings.py
+++ workspace/littlelemon/littlelemon/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    
 ]
 
 MIDDLEWARE = [
@@ -142,5 +145,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer', 
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+DJOSER={"USER_ID_FIELD":"username"}
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
-from rest_framework.decorators import api_view
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import response
 
 # Create your views here.
 def index(request):
@@ -20,3 +21,9 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return response({"message":"This view is protected"})
